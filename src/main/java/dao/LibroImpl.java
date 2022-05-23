e955adcd48d30fdbe922d7f60383d18df25a8a80
Jhanpool CUPE PEREZ <88795343+JhanpoolCupe@users.noreply.github.com>
2022-05-23 14:08:17 -0500

    GSON

---
+++ src/main/java/dao/LibroImpl.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package dao;
+
+import com.google.gson.Gson;
+import java.io.PrintWriter;
+import model.persona;
+
+/**
+ *
+ * @author cupej
+ */
+public class LibroImpl {
+
+    public static void main(String[] args) {
+
+        //Objeto con datos//
+        persona pr  = new persona();
+        pr.setNombre("Luis");
+        pr.setAño(20);
+        pr.setDeporte("futbol");
+
+        // Conversion con libro a Json
+        Gson gson = new Gson();
+        String jsonString = gson.toJson(pr);
+        System.out.println(jsonString);
+        
+        try (PrintWriter pw = new PrintWriter("Persona.json")){
+            pw.write(jsonString);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ src/main/java/model/persona.java
@@ -0,0 +1,32 @@
+
+package model;
+
+public class persona {
+    private String nombre;
+    private int año;
+    private String deporte;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getAño() {
+        return año;
+    }
+
+    public void setAño(int año) {
+        this.año = año;
+    }
+
+    public String getDeporte() {
+        return deporte;
+    }
+
+    public void setDeporte(String deporte) {
+        this.deporte = deporte;
+    }
+}
